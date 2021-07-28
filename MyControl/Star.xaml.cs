757631f8e4c52d0d5a9d3feb5814c6b54aacad37
Sari_nu95@itstep.edu.az <Sari_nu95@itstep.edu.az>
2021-07-28 16:50:44 +0400

    Add project files.

---
+++ MyControl/Star.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFFinalProject.MyControl
+{
+    /// <summary>
+    /// Interaction logic for Star.xaml
+    /// </summary>
+    public partial class Star : UserControl
+    {
+        private List<ToggleButton> stars;
+        public Star()
+        {
+            InitializeComponent();
+            stars = new List<ToggleButton>() {
+
+          T1,T2,T3,T4,T5
+            };
+        }
+        private void T1_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as ToggleButton).IsChecked = true;
+        }
+    }
+}
